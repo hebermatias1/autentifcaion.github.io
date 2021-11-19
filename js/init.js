23954fc4408382a6b86ae9eac4f9c512a8cf9189
Matias Cartagena Heber <83931183+hebermatias1@users.noreply.github.com>
2021-11-18 20:58:17 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0hg_uy5xD8dTcuemDYbNWT-In7DDcWsQ",
+    authDomain: "autenfi.firebaseapp.com",
+    projectId: "autenfi",
+    storageBucket: "autenfi.appspot.com",
+    messagingSenderId: "632991999795",
+    appId: "1:632991999795:web:1b98daa4b49af9ad658046",
+    measurementId: "G-SKJFBH6YJN"
+  };
